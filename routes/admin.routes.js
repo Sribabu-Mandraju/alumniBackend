caeed691a8c68035a1728d380f3eeb42e3464152
Yeluri Nakshatra <nakshatra.yeluri@gmail.com>
2025-02-14 16:00:19 +0530

    asdf

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,8 +2,7 @@ import express from 'express';
 import { createAdmin, loginAdmin, getAdminDetails } from '../controllers/admin.controllers.js';
 import { adminTokenCheck, isHod } from '../middlewares/admin.middleware.js';
 import { getAdminDetailsByToken } from '../controllers/admin.controllers.js';
-import { requestAlumniVerification } from '../controllers/admin.controllers.js'
-import { verifyAlumni } from '../controllers/admin.controllers.js'
+
 
 const router = express.Router();
 
@@ -11,8 +10,6 @@ router.post('/create', adminTokenCheck, isHod, createAdmin);
 router.post('/login', loginAdmin);
 router.get('/', adminTokenCheck,getAdminDetailsByToken);
 
-// Alumni verification routes
-router.post('/alumni/request-verification', adminTokenCheck, requestAlumniVerification);
-router.patch('/alumni/verify/:id', adminTokenCheck, isHod, verifyAlumni);
+
 
 export default router;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,9 @@
 // routes/authRoutes.js
 import express from "express";
 
-import { signUp,login } from "../controllers/authController.js";
+import { signUp, login, requestAlumniVerification, verifyAlumni } from '../controllers/authController.js';
+import { adminTokenCheck } from '../middlewares/admin.middleware.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 // Signup route
@@ -9,5 +11,11 @@ router.post("/signup", signUp);
 
 // Login route
 router.post("/login", login);
+
+
+// Alumni verification routes
+router.post('/alumni/request-verification',authMiddleware, requestAlumniVerification);
+router.patch('/alumni/verify/:id', adminTokenCheck, verifyAlumni);
+
 
 export default router;
